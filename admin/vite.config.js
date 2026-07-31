e0108f81df3b72aaf5be46b44f97628668fb1df8
Harshad Bajirao Khatale <khataleharshad26@gmail.com>
2026-08-01 01:53:03 +0530

    did deployment for admin

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: '/',
+
   server: {
     port: 5173,
     host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  }
-});
+    proxy: mode === 'development'
+      ? {
+          '/api': {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+            secure: false,
+          },
+        }
+      : undefined,
+  },
+}));
